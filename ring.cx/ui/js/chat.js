3825bb49d7fd1cdd92e497c6968e4fcef6943b06
Seva Ivanov <mail@sevaivanov.com>
2016-07-11 16:00:07 -0400

    chat ui: add contacts eventlListeners          add dynamic template for new contact          move moq to chatMoq.js libs:    add sweetalert2 for popups          move fontawesome from resources to libs

--- ring.cx/ui/js/chat.js
+++ ring.cx/ui/js/chat.js
@@ -1,82 +1,39 @@
 
-function randomInt(min, max)
+var chatHistory = document.getElementById("chatHistory"),
+    contacts = document.getElementById("contacts"),
+    contactsItem = document.getElementsByClassName("contactsItem"),
+    addContact = document.getElementById("addContact");
+
+function contactsItemClick()
 {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    alert(this.className);
+}
+for (var i = 0; i < contactsItem.length; i++)
+{
+    contactsItem[i].addEventListener('click', contactsItemClick, false);
 }
 
-function moqChatHistory()
+addContact.addEventListener("click", function()
 {
-    var chatHistory = document.getElementById("chatHistory");
+    html = "<div class='dynamicHtml'>" +
+        "<p>Name:<input type='text'></p>" +
+        "<p>Lastname:<input type='text'></p>" +
+        "<p>Ring ID:<input type='text'></p></div>";
 
-    for (var i = 0; i < 5; i++)
-    {
-        var chatHistoryItem = document.createElement("div"),
-            chatHistoryItemImage = document.createElement("div"),
-            chatHistoryItemText = document.createElement("div");
-
-        chatHistoryItem.className = "chatHistoryItem";
-        chatHistoryItemImage.className = "chatHistoryItemImage";
-        chatHistoryItemText.className = "chatHistoryItemText";
-
-        chatHistoryItemImage.innerHTML =
-           '<i class="fa fa-user fa-3x" aria-hidden="true"></i>';
-
-        // Conversation
-        text = "<p>Bla ";
-        for (var j = 0; j < randomInt(10, 500); j++)
+    swal({
+        title: 'New Contact',
+        html: html,
+        showCancelButton: true,
+        confirmButtonText: 'Submit',
+        showLoaderOnConfirm: true,
+        preConfirm: function()
         {
-            text += "bla ";
+            swal(
+                'Added!',
+                'New contact has been added.',
+                'success'
+            )
         }
-        chatHistoryItemText.innerHTML = (text += "</p>");
-
-        // Alter who talks
-        var cssFloat = "right";
-        if (i % 2 == 1) cssFloat = "left";
-        chatHistoryItemImage.style.cssFloat = cssFloat;
-        chatHistoryItemText.style.cssFloat = cssFloat;
-
-        chatHistoryItem.appendChild(chatHistoryItemImage);
-        chatHistoryItem.appendChild(chatHistoryItemText);
-
-        chatHistory.appendChild(chatHistoryItem);
-    }
-}
-
-function moqContacts()
-{
-    var contacts = document.getElementById("contacts"),
-        addContact = document.getElementById("addContact");
-
-    for (var i = 0; i < 5; i++)
-    {
-        var contactsItem = document.createElement("div"),
-            contactsItemImage = document.createElement("div"),
-            contactsItemText = document.createElement("div");
-            contactsItemOptions = document.createElement("div");
-
-        contactsItem.className = "contactsItem";
-        contactsItemImage.className = "contactsItemImage";
-        contactsItemText.className = "contactsItemText";
-        contactsItemOptions.className = "contactsItemOptions";
-
-        contactsItemImage.innerHTML =
-           '<i class="fa fa-user fa-3x" aria-hidden="true"></i>';
-
-        contactsItemText.innerHTML = "<p>Contact #" + i + "</p>";
-
-        contactsItemOptions.innerHTML =
-            '<i class="fa fa-ellipsis-v fa-2x" aria-hidden="true"></i>';
-
-        contactsItem.appendChild(contactsItemImage);
-        contactsItem.appendChild(contactsItemText);
-        contactsItem.appendChild(contactsItemOptions);
-
-        contacts.insertBefore(contactsItem, addContact);
-    }
-}
-
-// Acts like onload if the <script> has an async attribute
-
-moqChatHistory();
-moqContacts();
+    }).then(function() {})
+});
 
---
+++ ring.cx/ui/js/chatMoq.js
@@ -0,0 +1,82 @@
+
+function randomInt(min, max)
+{
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function moqChatHistory()
+{
+    var chatHistory = document.getElementById("chatHistory");
+
+    for (var i = 0; i < 5; i++)
+    {
+        var chatHistoryItem = document.createElement("div"),
+            chatHistoryItemImage = document.createElement("div"),
+            chatHistoryItemText = document.createElement("div");
+
+        chatHistoryItem.className = "chatHistoryItem";
+        chatHistoryItemImage.className = "chatHistoryItemImage";
+        chatHistoryItemText.className = "chatHistoryItemText";
+
+        chatHistoryItemImage.innerHTML =
+           '<i class="fa fa-user fa-3x" aria-hidden="true"></i>';
+
+        // Conversation
+        text = "<p>Bla ";
+        for (var j = 0; j < randomInt(10, 500); j++)
+        {
+            text += "bla ";
+        }
+        chatHistoryItemText.innerHTML = (text += "</p>");
+
+        // Alter who talks
+        var cssFloat = "right";
+        if (i % 2 == 1) cssFloat = "left";
+        chatHistoryItemImage.style.cssFloat = cssFloat;
+        chatHistoryItemText.style.cssFloat = cssFloat;
+
+        chatHistoryItem.appendChild(chatHistoryItemImage);
+        chatHistoryItem.appendChild(chatHistoryItemText);
+
+        chatHistory.appendChild(chatHistoryItem);
+    }
+}
+
+function moqContacts()
+{
+    var contacts = document.getElementById("contacts"),
+        addContact = document.getElementById("addContact");
+
+    for (var i = 0; i < 5; i++)
+    {
+        var contactsItem = document.createElement("div"),
+            contactsItemImage = document.createElement("div"),
+            contactsItemText = document.createElement("div");
+            contactsItemOptions = document.createElement("div");
+
+        contactsItem.className = "contactsItem";
+        contactsItemImage.className = "contactsItemImage";
+        contactsItemText.className = "contactsItemText";
+        contactsItemOptions.className = "contactsItemOptions";
+
+        contactsItemImage.innerHTML =
+           '<i class="fa fa-user fa-3x" aria-hidden="true"></i>';
+
+        contactsItemText.innerHTML = "<p>Contact #" + i + "</p>";
+
+        contactsItemOptions.innerHTML =
+            '<i class="fa fa-ellipsis-v fa-2x" aria-hidden="true"></i>';
+
+        contactsItem.appendChild(contactsItemImage);
+        contactsItem.appendChild(contactsItemText);
+        contactsItem.appendChild(contactsItemOptions);
+
+        contacts.insertBefore(contactsItem, addContact);
+    }
+}
+
+// Acts like onload if the <script> has an async attribute
+
+moqChatHistory();
+moqContacts();
+
